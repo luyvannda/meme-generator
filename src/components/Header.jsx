1dd3b6226b048b808aaad7962359c69af88a446e
luyvannda <philips.joe329@gmail.com>
2023-12-04 15:46:29 +0700

    create two divs and move left and right header to each respectively div for easily styling with flexbox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,16 @@ export default function Header() {
     <div
       id="header-component"
       className="container-fluid py-3 text-white d-flex flex-column flex-md-row justify-content-between align-items-center">
-      <h4><img src="/troll_face.png" alt="troll face icon" /> Meme Generator</h4>
-      <p>React Course - Project 3</p>
+
+      <div id="header-left"
+        className="container d-flex justify-content-center justify-content-md-start gap-2 ">
+        <img src="/troll_face.png" alt="troll face icon" />
+        <h4>Meme Generator</h4>
+      </div>
+
+      <div id="header-right" className="container d-flex justify-content-center justify-content-md-end pe-md-4">
+        <p>React Course - Project 3</p>
+      </div>
     </div>
   )
 }
