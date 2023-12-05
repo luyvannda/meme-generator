4975195d3ff61d9578bcce243efd6bf3a808abb9
luyvannda <philips.joe329@gmail.com>
2023-12-05 10:08:16 +0700

    use bootstrap to style Meme component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -5,20 +5,25 @@ import Row from 'react-bootstrap/Row';
 
 export default function Meme() {
   return (
-    <div className="container-fluid">
-      <Form>
+    <div id='meme-component' className="container-fluid mt-3">
+      <Form className='mt-4 mx-3 mx-md-4'>
         <Row>
-          <Col>
-            <Form.Control placeholder="First name" />
+          <Col sm={9} md={12} className='mb-3'>
+            <Form.Control placeholder="Shut up" />
           </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
+          <Col sm={9} md={12} className='mb-3'>
+            <Form.Control placeholder="and take my money" />
+          </Col>
+
+          <Col sm={9} md={12}>
+            <Button className='w-100 pt-2 pb-2 d-flex justify-content-center align-items-center gap-1' id='meme-button'
+              type="submit">
+              <p className='fs-6 fw-bold'>Get a new meme image </p>
+              <img src="/painting-icon.png" alt="a painting icon" />
+            </Button>
           </Col>
         </Row>
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
       </Form>
     </div>
   )
