d6f1ffd627be8a93923078cb2c5aae4ff3d7dfc9
luyvannda <philips.joe329@gmail.com>
2023-12-05 10:13:42 +0700

    fix form column for medium device

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -8,21 +8,21 @@ export default function Meme() {
     <div id='meme-component' className="container-fluid mt-3">
       <Form className='mt-4 mx-3 mx-md-4'>
         <Row>
-          <Col sm={9} md={12} className='mb-3'>
+          <Col sm={9} md={6} className='mb-3'>
             <Form.Control placeholder="Shut up" />
           </Col>
-          <Col sm={9} md={12} className='mb-3'>
+          <Col sm={9} md={6} className='mb-3'>
             <Form.Control placeholder="and take my money" />
           </Col>
-
-          <Col sm={9} md={12}>
-            <Button className='w-100 pt-2 pb-2 d-flex justify-content-center align-items-center gap-1' id='meme-button'
-              type="submit">
-              <p className='fs-6 fw-bold'>Get a new meme image </p>
-              <img src="/painting-icon.png" alt="a painting icon" />
-            </Button>
-          </Col>
         </Row>
+
+        <Col sm={9} md={12}>
+          <Button className='w-100 pt-2 pb-2 d-flex justify-content-center align-items-center gap-1' id='meme-button'
+            type="submit">
+            <p className='fs-6 fw-bold'>Get a new meme image </p>
+            <img src="/painting-icon.png" alt="a painting icon" />
+          </Button>
+        </Col>
 
       </Form>
     </div>
